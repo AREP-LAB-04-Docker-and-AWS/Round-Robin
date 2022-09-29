b2d73c47fd0436632a88ed64d2aee3f3166ba9aa
Brayan Macías <brayandresmt@outlook.com>
2022-09-28 21:06:34 -0500

    Adding Apache Spark

--- src/main/java/edu/eci/arep/roundRobin/SparkWebServer.java
+++ src/main/java/edu/eci/arep/roundRobin/SparkWebServer.java
@@ -1,8 +1,26 @@
 package edu.eci.arep.roundRobin;
 
+import spark.Request;
+import spark.Response;
+
+import static spark.Spark.*;
+
 public class SparkWebServer {
 
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        port(getPort());
+        get("/hello", SparkWebServer::getGreeting);
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+    private static String getGreeting(Request request, Response response) {
+        response.type("text/html");
+        return "Hello, Spark!";
     }
 }
