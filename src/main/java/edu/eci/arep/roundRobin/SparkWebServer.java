d060361526701b8a5cb85122ee9f7c0621e57422
Brayan Macías <brayandresmt@outlook.com>
2022-09-29 20:39:16 -0500

    Revert "Building RoundRobin feature"
    This reverts commit 841a36d0efa9b23832f3a14be673e2bd0f1a2aaf.

--- src/main/java/edu/eci/arep/roundRobin/SparkWebServer.java
+++ src/main/java/edu/eci/arep/roundRobin/SparkWebServer.java
@@ -1,29 +1,15 @@
 package edu.eci.arep.roundRobin;
 
-import edu.eci.arep.roundRobin.model.RoundRobin;
 import spark.Request;
 import spark.Response;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static spark.Spark.*;
 
 public class SparkWebServer {
 
-    private static RoundRobin roundRobin;
-
     public static void main( String[] args ) {
         port(getPort());
         get("/hello", SparkWebServer::getGreeting);
-
-        // Testing RoundRobin
-        roundRobin = new RoundRobin(SparkWebServer.getServicePorts());
-        System.out.println("Content: " + roundRobin.getContent());
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Using port: " + roundRobin.getNext());
-        }
     }
 
     private static int getPort() {
@@ -31,10 +17,6 @@ public class SparkWebServer {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567;
-    }
-
-    private static ArrayList<Integer> getServicePorts() {
-        return new ArrayList<Integer>(Arrays.asList(1111, 2222, 3333, 4444));
     }
 
     private static String getGreeting(Request request, Response response) {
